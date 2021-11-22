71c1066ce3ef74076695e447aec42cf6bfbaca46
a033565458v <a033565458v@S09-PC22>
2021-11-22 19:32:54 +0100

    Creado proyecto Java

---
+++ EjerciciosProgramacionJava/src/ejerciciosprogramacionjava/EjerciciosProgramacionJava.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto EjerciciosProgramacionJava - Archivo EjerciciosProgramacionJava.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * http://creativecommnos.org/licenses/by-nc-sa/4.0/
+ */
+
+package ejerciciosprogramacionjava;
+
+/**
+ *
+ * @author Raimon Vilar Morera <raimonv@gmail.com>
+ * @version 1.0
+ * @date 22 nov. 2021 19:31:43
+ */
+public class EjerciciosProgramacionJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
